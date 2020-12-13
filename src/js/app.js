93daa9fcfbb783dc262cbb129fa23f173b93a412
mileine <43147648+mileine@users.noreply.github.com>
2020-12-13 15:49:20 -0200

    Remove scroll from mobile menu screen

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,16 @@
 const links = document.querySelector('.nav')
 const btnMenu =  document.querySelector('.btn-menu')
-const btnClose = document.querySelector('btn-close')
+const btnClose = document.querySelector('.btn-close')
+const body = document.querySelector('body')
 
 function openMenu() {
   links.classList.add('display')
   btnMenu.classList.add('hidden')
+  body.classList.add('menu-mobile-visible')
 }
 
 function closeMenu() {
   links.classList.remove('display')
   btnMenu.classList.remove('hidden')
+  body.classList.remove('menu-mobile-visible')
 }
