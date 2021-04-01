63fcbf6e1655333b3bfba2a18c29ceedc6093763
mileine <43147648+mileine@users.noreply.github.com>
2021-04-01 18:43:12 -0300

    Validate e-mail

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,21 @@ function closeMenu() {
   btnMenu.classList.remove('hidden')
   body.classList.remove('menu-mobile-visible')
 }
+
+function validateEmail() {
+  const email = document.querySelector('.email')
+  const msg =  document.querySelector('.error-msg')
+  console.log(email.value)
+  if((email.value === '')||(!isEmailValid(email.value))) {
+    msg.innerHTML = 'Please insert a valid email'
+    email.classList.add('error')
+  } else {
+    email.classList.remove('error')
+    msg.innerHTML = ''
+  }
+}
+
+function isEmailValid(email) {
+ if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) return (true)
+ else return (false)
+}
